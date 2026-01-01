1f0e17b81bf3a6f198cb66316de05c7cc1785d6a
Jota-ato <julioc-01@outlook.com>
2026-01-01 16:58:22 -0600

    feat: initial version

--- app/ui/acme-logo.tsx
+++ app/ui/acme-logo.tsx
@@ -1,13 +1,13 @@
 import { GlobeAltIcon } from '@heroicons/react/24/outline';
-import { lusitana } from '@/app/ui/fonts';
+import { open_sans } from './fonts';
 
 export default function AcmeLogo() {
-  return (
-    <div
-      className={`${lusitana.className} flex flex-row items-center leading-none text-white`}
-    >
-      <GlobeAltIcon className="h-12 w-12 rotate-[15deg]" />
-      <p className="text-[44px]">Acme</p>
-    </div>
-  );
+    return (
+        <div
+            className={`${open_sans.className} flex flex-row items-center leading-none text-white`}
+        >
+            <GlobeAltIcon className="h-12 w-12 rotate-[15deg]" />
+            <p className="text-[44px]">Acme</p>
+        </div>
+    );
 }
