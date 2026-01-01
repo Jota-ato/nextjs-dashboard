bd022b987f72d5347d0b5dd6af67bd63bea131b3
Jota-ato <julioc-01@outlook.com>
2026-01-01 17:01:53 -0600

    lusitania

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,5 +1,4 @@
-import { Inter } from 'next/font/google';
-import { Open_Sans } from 'next/font/google';
+import { Inter, Lusitana } from 'next/font/google';
 
 export const inter = Inter({ subsets: ['latin'] });
-export const open_sans = Open_Sans({ subsets: ['latin'], weight: ['400', '700'] });
+export const lusitana = Lusitana({ subsets: ['latin'], weight: ['400', '700'] });
